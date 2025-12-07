231ab16939453fa2804ac28584cdd977031d61e8
SravanSakha <sravansakha@gmail.com>
2025-12-07 19:09:40 +0530

    Add redirect to index.html on script load
    Redirect to index.html when the script loads.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const DEFAULT_PIN = "1234";
 const pinInput = document.getElementById("adminPin");
 const loginBtn = document.getElementById("btnLogin");
 const msg = document.getElementById("message");
+window.location.href = "index.html";
+
 
 loginBtn.onclick = () => {
     const pin = pinInput.value.trim();
